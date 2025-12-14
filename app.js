eb5099072d633af12c538286d0195e0eaea2a175
SagiGalianW <sagigalianw@gmail.com>
2025-12-14 22:21:58 +0200

    just uncommneted important things

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const usersRouter = require("./routes/users");
 const rolesRouter = require("./routes/roles");
 const workersRouter = require("./routes/worker");
 const shiftsRouter = require("./routes/shifts");
-// const workerAssignmentsRouter = require("./routes/workerAssignments");
-// const adminRouter = require("./routes/admin");
+const workerAssignmentsRouter = require("./routes/workerAssignments");
+const adminRouter = require("./routes/admin");
 
 const { authenticate } = require("./middleware/authentication");
 // const PORT = 8080 || process.env.PORT;
@@ -43,8 +43,8 @@ app.use("/user", usersRouter);
 app.use("/role", rolesRouter);
 app.use("/worker", workersRouter);
 app.use("/shift", shiftsRouter);
-// app.use("/workerAssignments", workerAssignmentsRouter);
-// app.use("/admin", adminRouter);
+app.use("/workerAssignments", workerAssignmentsRouter);
+app.use("/admin", adminRouter);
 
 // status route
 app.get("/status", authenticate, (req, res) => {
